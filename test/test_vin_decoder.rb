aaac8bcda364e3b9046059061be55158437d9fa7
Matheus Porto Pereira <matheus.pereira11@gmail.com>
2021-01-23 15:06:16 -0300

    refactor(vin_decoder_test): set vins as global variables to improve reuse

--- test/test_vin_decoder.rb
+++ test/test_vin_decoder.rb
@@ -3,21 +3,27 @@ require_relative "../features/vin_decoder"
 
 # with at least unit test is more simplest to validate methods
 class TestVinDecoder < Test::Unit::TestCase
+  def setup
+    @valid_vin = "2NKWL00X16M149834"
+    @invalid_vin = "INKDLUOX33R385016"
+  end
+
   def test_calculate_check_digit
-    # with valid vin
-    vin = "2NKWL00X16M149834"
-    chk_digit = calculate_check_digit(vin)
-    assert_equal chk_digit.to_s, vin[8], "check digit should return #{vin[8]}"
+    @valid_vin
+    chk_digit = calculate_check_digit(@valid_vin)
+    assert_equal chk_digit.to_s, @valid_vin[8], "check digit should return #{@valid_vin[8]}"
   end
 
   def test_valid_vin
     # should validate vin chars
-    valid_vin = "2NKWL00X16M149834"
-    validation = valid_vin(valid_vin)
+    validation = valid_vin(@valid_vin)
     assert_true(validation, "Vin is valid")
 
-    invalid_vin = "INKDLUOX33R385016"
-    validation = valid_vin(invalid_vin)
+    validation = valid_vin(@invalid_vin)
     assert_false(validation, "Vin is invalid")
   end
+
+  # def test_generate_valid_vin
+  #
+  # end
 end
